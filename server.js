9e4af222416005b0e37c92f198339d0d3513ee08
rania <ranianadine.benhadjyoussef@esprit.tn>
2024-03-21 10:04:39 +0100

    productionyinl

--- server.js
+++ server.js
@@ -1,18 +1,26 @@
 "use strict";
 
-var PORT = process.env['PORT'] || 4000;
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { createServer } from 'http';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const PORT = process.env.PORT || 4000;
 
 import express from 'express';
-import path from 'path';
-
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/mon-projet'));
+app.use(express.static(path.join(__dirname, 'dist', 'mon-projet')));
 
-app.get('/*', function(req,res) {
-     res.sendFile(path.join(__dirname+'/dist/mon-projet/index.html'));
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'dist', 'mon-projet', 'index.html'));
 });
-app.listen(PORT, function () {
-    console.log("Node Express server listening on http://localhost:".concat(PORT));
+
+const server = createServer(app);
+
+server.listen(PORT, function() {
+    console.log(`Node Express server listening on http://localhost:${PORT}`);
 });
