622cb9a6f28e0011571eef9a26ad6093269dcd49
rania <ranianadine.benhadjyoussef@esprit.tn>
2024-03-21 09:01:19 +0100

    productionmtp

--- server.ts
+++ server.ts
@@ -10,7 +10,10 @@ const app = express();
 app.use(express.static(__dirname + '/dist/chatcount-front/browser'));
 
 app.get('/*', function(req,res) {
-     res.sendFile(path.join(__dirname+'/dist/chatcount-front/browser/index.html'));
+  res.sendFile(path.resolve(__dirname, 'dist', 'chatcount-front', 'browser', 'index.html'));
+});
+app.use(function(req, res, next) {
+  res.status(404).send("Sorry, can't find that!");
 });
 // Start the server
 app.listen(PORT, () => {
