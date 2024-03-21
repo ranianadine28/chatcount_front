065ffaf10a2951abdcacf954a13df1a2edc8d776
rania <ranianadine.benhadjyoussef@esprit.tn>
2024-03-21 10:12:59 +0100

    productionyinln

--- server.js
+++ server.js
@@ -3,20 +3,20 @@
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import { createServer } from 'http';
+import express from 'express'; // Importation d'express
+import path from 'path'; // Importation de path
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const PORT = process.env.PORT || 4000;
 
-import express from 'express';
-
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist', 'mon-projet')));
+app.use(express.static(path.join(__dirname, 'dist', 'chatcount-front', 'browser')));
 
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'dist', 'mon-projet', 'index.html'));
+    res.sendFile(path.join(__dirname, 'dist', 'chatcount-front', 'browser', 'index.html'));
 });
 
 const server = createServer(app);
