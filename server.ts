d1bc017b02fccdb22fe0a673301af6c98448c82d
rania <ranianadine.benhadjyoussef@esprit.tn>
2024-03-21 09:13:09 +0100

    productio

--- server.ts
+++ server.ts
@@ -1,20 +1,20 @@
-const express = require('express');
-const path = require('path');
+import express, { Request, Response, NextFunction } from 'express';
+import path from 'path';
+
 const PORT = process.env['PORT'] || 4000;
-
-
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/chatcount-front/browser'));
+app.use(express.static(path.join(__dirname, '/dist/chatcount-front/browser')));
 
-app.get('/*', function(req,res) {
-  res.sendFile(path.resolve(__dirname, 'dist', 'chatcount-front', 'browser', 'index.html'));
+app.get('/*', function(req: Request, res: Response) {
+  res.sendFile(path.join(__dirname, '/dist/chatcount-front/browser/index.html'));
 });
-app.use(function(req, res, next) {
+
+app.use(function(req: Request, res: Response, next: NextFunction) {
   res.status(404).send("Sorry, can't find that!");
 });
-// Start the server
+
 app.listen(PORT, () => {
   console.log(`Node Express server listening on http://localhost:${PORT}`);
 });
