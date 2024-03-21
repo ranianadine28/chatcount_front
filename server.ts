444f914940e978f6105556205949eccb88ffc40b
rania <ranianadine.benhadjyoussef@esprit.tn>
2024-03-21 07:53:31 +0100

    prod

---
+++ server.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import { join } from 'path';
+import { Request, Response } from 'express';
+
+const app = express();
+const PORT = process.env['PORT'] || 4000;
+
+const DIST_FOLDER = join(process.cwd(), 'dist/your-chatcount-front');
+
+// Serve static files
+app.use(express.static(DIST_FOLDER));
+
+// All other routes redirect to the index.html
+app.get('*', (req: Request, res: Response) => {
+
+  res.sendFile(join(DIST_FOLDER, 'index.html'));
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Node Express server listening on http://localhost:${PORT}`);
+});
