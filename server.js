41d7c2d9dae0874947fb6d70137d82a61bba3bc0
rania <ranianadine.benhadjyoussef@esprit.tn>
2024-03-21 10:01:09 +0100

    productionyin

--- server.js
+++ server.js
@@ -2,8 +2,9 @@
 
 var PORT = process.env['PORT'] || 4000;
 
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import path from 'path';
+
 
 const app = express();
 
