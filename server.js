b6437b7d8c9c72ab45cd37c5f1c063f01679f92f
rania <ranianadine.benhadjyoussef@esprit.tn>
2024-04-01 23:50:51 +0100

    renderinglm

--- server.js
+++ server.js
@@ -1,15 +1,29 @@
+"use strict";
+
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { createServer } from 'http';
 import express from 'express';
 import path from 'path';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const PORT = process.env.PORT || 4000;
+
 const app = express();
 
-app.use(express.static(path.join(__dirname, '/dist/chatcount-front')));
-
+// Handle all routes first
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, '/dist/chatcount-front/index.html'));
+  console.log('Received request for:', req.url);
+  res.sendFile(path.join(__dirname, 'dist', 'chatcount-front', 'index.html'));
 });
 
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+// Serve static files
+app.use(express.static(path.join(__dirname, 'dist', 'chatcount-front')));
+
+const server = createServer(app);
+
+server.listen(PORT, function() {
+  console.log(`Node Express server listening on http://localhost:${PORT}`);
 });
