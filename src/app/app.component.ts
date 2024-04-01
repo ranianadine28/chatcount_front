7947edb0ae5af13b27979e2b436e9f61c4ec5c3d
rania <ranianadine.benhadjyoussef@esprit.tn>
2024-04-02 00:30:27 +0100

    renderi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { EnvironmentService } from '../enviroment.service';
 
 @Component({
   selector: 'app-root',
@@ -7,10 +6,5 @@ import { EnvironmentService } from '../enviroment.service';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  baseHref: string | undefined;
-  constructor(private environmentService: EnvironmentService) {}
-
-  ngOnInit() {
-    this.baseHref = this.environmentService.getBaseHref();
-  }
+ 
 }
