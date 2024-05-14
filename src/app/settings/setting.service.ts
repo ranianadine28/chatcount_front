929ab5c36bcbb2cf6f7319d72b318f1e32a5965b
rania <ranianadine.benhadjyoussef@esprit.tn>
2024-05-14 18:28:43 +0100

    gestion_mapping

--- src/app/settings/setting.service.ts
+++ src/app/settings/setting.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../environment/environment';
-import { Observable } from 'rxjs';
+import { Observable, catchError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -87,5 +87,57 @@ export class SettingsService {
   deletelabel5(folderId: string): Observable<any> {
     return this.http.delete<any>(`${this.apiUrl}/label/deleteLabel5/${folderId}`);
   }
-
+  getCsvData(): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/mots/getCsvData`)
+      .pipe(
+        catchError(this.handleError) 
+      );
+  }
+  getPatternsData(): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/patterns/getpatterns`)
+      .pipe(
+        catchError(this.handleError) 
+      );
+  }
+  insertCsvData(newRow: any): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/mots/insertCsv`, newRow)
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
+  insertPatternData(newRow: any): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/patterns/insertligne`, newRow)
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error);
+    return Promise.reject(error.message || error);
+  }
+  deleteCsvData(rowIndex: number, columnIndex: number): Observable<any> {
+    const options = {
+      body: { rowIndex, columnIndex } 
+    };
+    return this.http.delete<any>(`${this.apiUrl}/mots/deleteCsv`, options);
+  }
+  deletePatternData(rowIndex: number, columnIndex: number): Observable<any> {
+    const options = {
+      body: { rowIndex, columnIndex } 
+    };
+    return this.http.delete<any>(`${this.apiUrl}/patterns/deletePattern`, options);
+  }
+  
+  updateCsvData(rowIndex: number, columnIndex: number, newValue: string): Observable<any> {
+    return this.http.put<any>(`${this.apiUrl}/mots/updateCsv`, { rowIndex, columnIndex, newValue });
+  }
+  updatePatternData(rowIndex: number, columnIndex: number, newValue: string): Observable<any> {
+    return this.http.put<any>(`${this.apiUrl}/patterns/updatePattern`, { rowIndex, columnIndex, newValue });
+  }
+  addColumn(newColumn: string): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/mots/addColumn`, { newColumn });
+  }
+  updateCsvDataColonne(newColumn: string): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/updateCsvDataColonne`, { newColumn });
+  }
 }
--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -10,7 +10,10 @@ import { NgbDropdown } from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./settings.component.css']
 })
 export class SettingsComponent implements OnInit {
-  labels: any[] = [];
+  fecSettingsButtonColor: string = '#2541b200';
+  keywordsButtonColor: string = '#2541b200';
+  patternsButtonColor: string = '#2541b200';
+    labels: any[] = [];
   labels2: any[] = [];
   labels3: any[] = [];
   labels4: any[] = [];
@@ -30,20 +33,250 @@ showIcon :boolean=false;
   searchRootId: string = '';
   searchLabel: string = '';
   @ViewChild('dropdown') dropdown: NgbDropdown | undefined;
+  showFECSettings: boolean = false;
+  showKeywords: boolean = false;
+  showPatterns: boolean = false;
+  isEditing: boolean[][] = [];
+  newColumnName: string = '';
+  patternsData: any;
 
-
+  csvData: any; 
   newLabelRootId: string = '';
   newLabelName: string = '';
-
+  newRow: any;
   constructor(private settingsService: SettingsService,
     private _snackBar: MatSnackBar,
 
   ) { }
 
   ngOnInit(): void {
+    this.getPatterns();
+
+    this.getCsvData();
+  }
+  getPatterns(): void {
+    console.log("enaaaaaaaaaaaaaaa");
+    this.settingsService.getPatternsData()
+      .subscribe(
+        (data) => {
+          this.patternsData = data;
+          console.log('Patterns Data:', this.patternsData);
+        },
+        (error) => {
+          console.error('Error fetching patterns:', error);
+          // Handle error as per your need
+        }
+      );
+  }
+  getCsvData(): void {
+    this.settingsService.getCsvData()
+      .subscribe(data => {
+        this.csvData = data;
+        this.newRow = new Array(this.csvData?.titre.split(';').length).fill('');
+        this.initializeEditingArray();
+
+      });
+  }
+ 
+  initializeEditingArray(): void {
+    this.isEditing = new Array(this.csvData?.contenu.length).fill(false).map(() => new Array(this.csvData?.titre.split(';').length).fill(false));
   }
 
+  toggleEdit(row: number, col: number): void {
+    this.isEditing![row][col] = !this.isEditing![row][col];
+  }
 
+  insertRow(): void {
+    this.settingsService.insertCsvData(this.newRow)
+      .subscribe(() => {
+console.log("nnnnnnnnnnnnnnnn");
+this._snackBar.open('Vous avez ajouter le mot clés avec succès.', 'Fermer', {
+  panelClass: ['redNoMatch'] ,
+
+  duration: 5000,
+  horizontalPosition: 'center',
+  verticalPosition: 'top'
+});
+        this.getCsvData();
+      });
+  }
+  isFormValid(): boolean {
+    return this.newRow.every((value:any) => value.trim() !== '');
+  }
+  insertRowPaatern(): void {
+    this.settingsService.insertPatternData(this.newRow)
+      .subscribe(() => {
+console.log("nnnnnnnnnnnnnnnn");
+this._snackBar.open('Vous avez ajouter le question avec succès.', 'Fermer', {
+  panelClass: ['redNoMatch'] ,
+
+  duration: 5000,
+  horizontalPosition: 'center',
+  verticalPosition: 'top'
+});
+        this.getPatterns();
+      });
+  }
+  deleteCsvData(rowIndex: number, columnIndex: number): void {
+    this.settingsService.deleteCsvData(rowIndex, columnIndex)
+      .subscribe(
+        response => {
+          if (response && response.message) {
+            console.log('Cellule supprimée avec succès:', response.message);
+            this._snackBar.open('Cellule supprimée avec succès.', 'Fermer', {
+              panelClass: ['redNoMatch'],
+              duration: 5000,
+              horizontalPosition: 'center',
+              verticalPosition: 'top'
+            });
+          } else {
+            console.log('Réponse de suppression invalide:', response);
+          }
+        },
+        error => {
+          console.error('Erreur lors de la suppression de la cellule:', error);
+          this._snackBar.open('Erreur lors de la suppression de la cellule.', 'Fermer', {
+            panelClass: ['redNoMatch'],
+            duration: 5000,
+            horizontalPosition: 'center',
+            verticalPosition: 'top'
+          });
+        }
+      );
+}
+deletePatternData(rowIndex: number, columnIndex: number): void {
+  this.settingsService.deletePatternData(rowIndex, columnIndex)
+    .subscribe(
+      response => {
+        if (response && response.message) {
+          console.log('Cellule supprimée avec succès:', response.message);
+          this._snackBar.open('Cellule supprimée avec succès.', 'Fermer', {
+            panelClass: ['redNoMatch'],
+            duration: 5000,
+            horizontalPosition: 'center',
+            verticalPosition: 'top'
+          });
+        } else {
+          console.log('Réponse de suppression invalide:', response);
+        }
+      },
+      error => {
+        console.error('Erreur lors de la suppression de la cellule:', error);
+        this._snackBar.open('Erreur lors de la suppression de la cellule.', 'Fermer', {
+          panelClass: ['redNoMatch'],
+          duration: 5000,
+          horizontalPosition: 'center',
+          verticalPosition: 'top'
+        });
+      }
+    );
+}
+
+  updateCellValue(rowIndex: number, columnIndex: number, event: any): void {
+    const newValue = event?.target?.value ?? '';
+    this.settingsService.updateCsvData(rowIndex, columnIndex, newValue)
+      .subscribe(
+        response => {
+          console.log('Cellule mise à jour avec succès :', response);
+          this._snackBar.open('Cellule mise à jour avec succès.', 'Fermer', {
+            panelClass: ['redNoMatch'] ,
+            duration: 5000,
+            horizontalPosition: 'center',
+            verticalPosition: 'top'
+          });
+        },
+        error => {
+          console.error('Erreur lors de la mise à jour de la cellule :', error);
+          this._snackBar.open('Erreur lors de la mise à jour de la cellule.', 'Fermer', {
+            panelClass: ['redNoMatch'] ,
+            duration: 5000,
+            horizontalPosition: 'center',
+            verticalPosition: 'top'
+          });
+        }
+      );
+  }
+  updatePatternValue(rowIndex: number, columnIndex: number, event: any): void {
+    const newValue = event?.target?.value ?? '';
+    this.settingsService.updatePatternData(rowIndex, columnIndex, newValue)
+      .subscribe(
+        response => {
+          console.log('Cellule mise à jour avec succès :', response);
+          this._snackBar.open('Cellule mise à jour avec succès.', 'Fermer', {
+            panelClass: ['redNoMatch'] ,
+            duration: 5000,
+            horizontalPosition: 'center',
+            verticalPosition: 'top'
+          });
+        },
+        error => {
+          console.error('Erreur lors de la mise à jour de la cellule :', error);
+          this._snackBar.open('Erreur lors de la mise à jour de la cellule.', 'Fermer', {
+            panelClass: ['redNoMatch'] ,
+            duration: 5000,
+            horizontalPosition: 'center',
+            verticalPosition: 'top'
+          });
+        }
+      );
+  }
+  addColumn(): void {
+    if (this.newColumnName.trim() !== '') {
+      this.settingsService.addColumn(this.newColumnName)
+        .subscribe(
+          response => {
+            console.log('Nouvelle colonne ajoutée avec succès :', response);
+            this._snackBar.open('Nouvelle colonne ajoutée avec succès.', 'Fermer', {
+              duration: 5000,
+              horizontalPosition: 'center',
+              verticalPosition: 'top'
+            });
+          },
+          error => {
+            console.error('Erreur lors de l\'ajout de la colonne :', error);
+            this._snackBar.open('Erreur lors de l\'ajout de la colonne.', 'Fermer', {
+              duration: 5000,
+              horizontalPosition: 'center',
+              verticalPosition: 'top'
+            });
+          }
+        );
+    } else {
+      this._snackBar.open('Veuillez saisir un nom pour la nouvelle colonne.', 'Fermer', {
+        duration: 5000,
+        horizontalPosition: 'center',
+        verticalPosition: 'top'
+      });
+    }
+  }
+
+  toggleFECSettings() {
+    this.fecSettingsButtonColor = '#5876ec'; // Couleur bleue avec transparence
+    this.keywordsButtonColor = '#2541b200';
+    this.patternsButtonColor = '#2541b200';
+    this.showFECSettings = true;
+    this.showKeywords = false;
+    this.showPatterns = false;
+  }
+
+  toggleKeywords() {
+    this.keywordsButtonColor = '#5876ec'; // Couleur bleue avec transparence
+    this.fecSettingsButtonColor = '#2541b200';
+    this.patternsButtonColor = '#2541b200';
+    this.showFECSettings = false;
+    this.showKeywords = true;
+    this.showPatterns = false;
+  }
+
+  togglePatterns() {
+    this.patternsButtonColor = '#5876ec'; // Couleur bleue avec transparence
+    this.keywordsButtonColor = '#2541b200';
+    this.fecSettingsButtonColor = '#2541b200';
+    this.showFECSettings = false;
+    this.showKeywords = false;
+    this.showPatterns = true;
+
+  }
   toggleLabels2(): void {
     this.showLabels2 = !this.showLabels2;
     if (this.showLabels2) {
@@ -505,4 +738,5 @@ showIcon :boolean=false;
       }
     );
   }
+
 }
